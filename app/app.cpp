06a354529b1433b897be0edfd951d864780ac2dc
Corey Richardson <richardsoncorey04@gmail.com>
2025-10-23 17:17:30 +0100

    Integrate level loading and display in main app to test

--- app/app.cpp
+++ app/app.cpp
@@ -2,8 +2,21 @@
 //
 
 #include <iostream>
+#include <string>
+
+#include "Tile.h"
+#include "Grid.h"
+#include "LevelLoader.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::string filename = "levels/level-example.txt";
+
+    Grid level = Grid(LevelLoader::loadLevel(filename));
+    level.display();
+
+    std::cout << "Start Coords: " << level.getStartCoords().x << ", " << level.getStartCoords().y << std::endl;
+    std::cout << "End Coords:   " << level.getEndCoords().x << ", " << level.getEndCoords().y << std::endl;
+
+    std::cout << "Walkable Tiles: " << level.getRemainingWalkableTiles() << std::endl;
 }
