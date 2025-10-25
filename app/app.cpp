409c8874b342828788a4a208ff16a12302eef9e9
Corey Richardson <richardsoncorey04@gmail.com>
2025-10-25 16:20:31 +0100

    Improved error reporting in app.cpp

--- app/app.cpp
+++ app/app.cpp
@@ -5,6 +5,7 @@
 #include <string>
 
 #include "Game.h"
+#include "LevelLoader.h"
 
 int main()
 {
@@ -13,6 +14,21 @@ int main()
 	try {
 		Game game(levelManifestFilename);
 		game.run();
+	}
+	catch (const LevelLoadException& e) {
+		std::cerr << "A required game file could not be loaded!" << std::endl;
+		std::cerr << e.what() << std::endl;
+		return 2;
+	}
+	catch (const std::runtime_error& e) {
+		std::cerr << "A runtime error caused an exception!" << std::endl;
+		std::cerr << e.what() << std::endl;
+		return 3;
+	}
+	catch (const std::exception& e) {
+		std::cerr << "An unknown error caused an exception!" << std::endl;
+		std::cerr << e.what() << std::endl;
+		return 4;
 	}
 	catch (...) {
 		std::cerr << "Game crashed :(" << std::endl;
